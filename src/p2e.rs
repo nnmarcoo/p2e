b50c2b718ad0c95ce7dbc538ab3355e7fdb7951b
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-02-10 00:02:47 -0500

    hello world

--- src/p2e.rs
+++ src/p2e.rs
@@ -1,14 +1,25 @@
 use eframe::{App, CreationContext, Frame};
-use egui::Context;
+use egui::{CentralPanel, Context};
 
-pub struct P2e {}
+pub struct P2e {
+  counter: u32,
+}
 
 impl P2e {
     pub fn new(_cc: &CreationContext<'_>) -> Self {
-        Self {}
+        Self {
+          counter: 0,
+        }
     }
 }
 
 impl App for P2e {
-    fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {}
+    fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {
+      CentralPanel::default().show(_ctx, |ui| {
+        ui.label(format!("Counter: {}", self.counter));
+        if ui.button("Increment").clicked() {
+          self.counter += 1;
+        }
+      });
+    }
 }
