82ec3805885c7fdf9510f819c1a860a406910db2
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-02-09 23:58:05 -0500

    fmt

--- src/p2e.rs
+++ src/p2e.rs
@@ -1,20 +1,14 @@
 use eframe::{App, CreationContext, Frame};
 use egui::Context;
 
-
-
-pub struct P2e {
-
-}
+pub struct P2e {}
 
 impl P2e {
     pub fn new(_cc: &CreationContext<'_>) -> Self {
-        Self {
-        }
+        Self {}
     }
 }
 
 impl App for P2e {
-  fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {
-  }
+    fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {}
 }
