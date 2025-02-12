45c79f07358fac20dc23cef62a1d5c024edadbb9
nicksyds <nicholassydorenko@gmail.com>
2025-02-11 19:01:39 -0500

    Update NickSim.py

--- src/NickSim.py
+++ src/NickSim.py
@@ -26,4 +26,30 @@ R = R.flatten()
 G = G.flatten()
 B = B.flatten()
 
-print(R)
+error_threshold = 1e-20
+initial_guess = [1,.5,0,0,.5,1,1,.5,2]#The initial guess is arbitrary but still vitally important, as our model progresses we can have a more efficient starting point
+zero_error = np.zeros(len(R))
+print(zero_error)
+
+#Learn Function: We input our data "x" and our any number of number of terms for combinations of sinusoids and polynomials
+#The output of this function is a new model which will be tested for accuracy in "test"
+def learn(x, *params):
+    num_sinusoids = len(params) // 2  # Half the terms are for sinusoids
+    sinusoids = sum([params[i] * np.sin(params[i+1] * x + params[i+2]) for i in range(0, num_sinusoids * 3, 3)])
+    polynomial = sum([params[i+num_sinusoids*3] * x**(i//3) for i in range(num_sinusoids*3, len(params))])
+    return sinusoids + polynomial
+
+#Test Function: We have recieved a new model from "learn". We will now take the RMS of every point and find the mean and return it.
+#This error is what we want to minimize in order to achieve 100% accuracy of our equation
+def test(x,y,new_model,*params):
+    y_hat = new_model(x,*params)
+    error = np.mean((y-y_hat)**2)
+    return error
+
+params_opt = initial_guess
+test_error = test(len(R),R)
+errors = [test_error]
+error = test(len(R),R)
+
+#The Training
+
